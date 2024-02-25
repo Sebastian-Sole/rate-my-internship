bc3ee41c0534b1be61ec972b256f6b834bc4bb28
Sebastian Sole <sebastrs@stud.ntnu.no>
2024-02-24 23:01:38 -0500

    feat: company card

---
+++ src/lib/components/companyCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button } from './ui/button';
+	import * as Card from './ui/card';
+
+	export let companyName: string;
+	export let description: string;
+</script>
+
+<Card.Root class="mx-10 my-5 w-[380px]">
+	<Card.Header>
+		<Card.Title>{companyName}</Card.Title>
+		<Card.Description>{description}</Card.Description>
+	</Card.Header>
+	<Card.Content class="grid gap-4"></Card.Content>
+	<Card.Footer>
+		<Button class="w-full">See internships</Button>
+	</Card.Footer>
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,29 @@
-<script>
+<script lang="ts">
+	import CompanyCard from '$lib/components/companyCard.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Input } from '$lib/components/ui/input';
 	import H1 from '$lib/components/ui/typography/h1.svelte';
+	import Small from '$lib/components/ui/typography/small.svelte';
+
+	let inputValue: string = '';
+
+	const handleChange = (e: any) => {
+		inputValue = e.target.value;
+	};
 </script>
 
-<div class="mt-[50px] flex items-center justify-center pb-96">
-	<div>
+<div class="mt-[50px] flex flex-col items-center justify-center pb-96">
+	<div class="flex w-full flex-col items-center">
 		<H1>Rate My Internship 🎉</H1>
-		<Button>Button</Button>
+		<Small className="mt-3">Søk på hundrevis av bedrifter og sommerjobber</Small>
+		<Input placeholder="Bekk..." class="mt-6 w-[80%]" on:change={(e) => handleChange(e)}></Input>
+	</div>
+	<div class="grid-cols-3 gap-4 xl:grid">
+		<CompanyCard companyName="Bekk" description="3 Reviews" />
+		<CompanyCard companyName="Capra" description="3 Reviews" />
+		<CompanyCard companyName="Knowit" description="3 Reviews" />
+		<CompanyCard companyName="Sportradar" description="3 Reviews" />
+		<CompanyCard companyName="Netlight" description="3 Reviews" />
 	</div>
 </div>
